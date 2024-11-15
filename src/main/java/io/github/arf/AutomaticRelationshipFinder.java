b7bba2b06bb668ebfcdf5575b7f8cd7e21bd4e52
NoelToy <noeltoy21@gmail.com>
2024-11-15 22:24:14 +0530

    --Detecting yes/no y/n t/f as boolean --Ignoring columns with specific patters --Parallel training data

--- src/main/java/io/github/arf/AutomaticRelationshipFinder.java
+++ src/main/java/io/github/arf/AutomaticRelationshipFinder.java
@@ -15,6 +15,10 @@ import io.github.fwm.WordMatcher;
 import io.github.fwm.lib.enums.MatchType;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class AutomaticRelationshipFinder<T> {
@@ -23,12 +27,16 @@ public class AutomaticRelationshipFinder<T> {
     private final double columnNameConfidence;
     private final double dataConfidence;
     private final List<DataTypes> ignoreDatatypes;
+    private final List<String> ignoreColumnNamePatterns;
+    private final int threadCount;
 
-    private AutomaticRelationshipFinder(List<Table<T>> tables, double columnNameConfidence, double dataConfidence, List<DataTypes> ignoreDatatypes){
+    private AutomaticRelationshipFinder(List<Table<T>> tables, double columnNameConfidence, double dataConfidence, List<DataTypes> ignoreDatatypes,List<String> ignoreColumnNamePatterns,int threadCount){
         this.tables = tables;
         this.columnNameConfidence = columnNameConfidence;
         this.dataConfidence = dataConfidence;
         this.ignoreDatatypes = ignoreDatatypes;
+        this.ignoreColumnNamePatterns = ignoreColumnNamePatterns;
+        this.threadCount = threadCount;
     }
 
     public static class AutomaticRelationshipFinderBuilder<T>{
@@ -36,9 +44,13 @@ public class AutomaticRelationshipFinder<T> {
         private double columnNameConfidence = 0.4;
         private double dataConfidence = 0.6;
         private final List<DataTypes> ignoreDatatypes = new ArrayList<>();
+        private final List<String> ignoreColumnNamePatterns = new ArrayList<>();
+        private int threadCount;
 
         public AutomaticRelationshipFinderBuilder(List<Table<T>> tables){
             this.tables = tables;
+            int threadCount = Runtime.getRuntime().availableProcessors()-1;
+            this.threadCount = Math.min(this.tables.size(), threadCount);
         }
 
         public AutomaticRelationshipFinderBuilder<T> setColumnNameConfidence(double value){
@@ -62,8 +74,23 @@ public class AutomaticRelationshipFinder<T> {
             return this;
         }
 
+        public AutomaticRelationshipFinderBuilder<T> setIgnoreColumnNamePatterns(List<String> ignoreColumnNamePatterns){
+            this.ignoreColumnNamePatterns.addAll(ignoreColumnNamePatterns);
+            return this;
+        }
+
+        public AutomaticRelationshipFinderBuilder<T> setThreadCount(int threadCount){
+            this.threadCount = threadCount;
+            return this;
+        }
+
         public AutomaticRelationshipFinder<T> builder(){
-            return new AutomaticRelationshipFinder<T>(this.tables,this.columnNameConfidence,this.dataConfidence,this.ignoreDatatypes);
+            return new AutomaticRelationshipFinder<>(this.tables,
+                    this.columnNameConfidence,
+                    this.dataConfidence,
+                    this.ignoreDatatypes,
+                    this.ignoreColumnNamePatterns,
+                    this.threadCount);
         }
 
         private boolean isValidRange(double value) {
@@ -75,23 +102,34 @@ public class AutomaticRelationshipFinder<T> {
     public List<Relationship> findRelationShip(){
         Map<String,RecordAnalysisResult> tableAnalyzesResult = new HashMap<>();
 //        Analyzing each table using FTA
-        tables.forEach(table -> {
-            AnalyzerContext context = new AnalyzerContext(null, DateTimeParser.DateResolutionMode.Auto, table.name(), ListToArray.convertToArray(table.columns()));
-            TextAnalyzer template = new TextAnalyzer(context);
-            RecordAnalyzer analysis = new RecordAnalyzer(template);
-            for (String [] data : ListToArray.convertTo2DArray(table.data())) {
-                try {
-                    analysis.train(data);
-                } catch (FTAPluginException | FTAUnsupportedLocaleException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            try {
-                tableAnalyzesResult.put(table.name(),analysis.getResult());
-            } catch (FTAPluginException | FTAUnsupportedLocaleException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        ExecutorService executor = Executors.newFixedThreadPool(this.threadCount);
+        List<CompletableFuture<Void>> futures = tables.stream()
+                .map(table -> CompletableFuture.runAsync(() -> {
+                    AnalyzerContext context = new AnalyzerContext(null, DateTimeParser.DateResolutionMode.Auto, table.name(), ListToArray.convertToArray(table.columns()));
+                    TextAnalyzer template = new TextAnalyzer(context);
+                    RecordAnalyzer analysis = new RecordAnalyzer(template);
+                    for (String[] data : ListToArray.convertTo2DArray(table.data())) {
+                        try {
+                            analysis.train(data);
+                        } catch (FTAPluginException | FTAUnsupportedLocaleException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    try {
+                        synchronized (tableAnalyzesResult) {
+                            tableAnalyzesResult.put(table.name(), analysis.getResult());
+                        }
+                    } catch (FTAPluginException | FTAUnsupportedLocaleException e) {
+                        throw new RuntimeException(e);
+                    }
+                }, executor)).toList();
+
+        for (CompletableFuture<Void> future : futures) {
+            future.join();
+        }
+
+        executor.shutdown();
 //        Checking datatypes and forming intermediate relationships
         List<List<IntermediateRelationship>> intermediateTableRelationships = new ArrayList<>();
         List<String> ignoreDatatypeList = ignoreDatatypes.stream().map(Enum::name).toList();
@@ -110,14 +148,11 @@ public class AutomaticRelationshipFinder<T> {
                 List<IntermediateRelationship> intermediateRelationships = IntStream.range(0, columnDataTypesA.size())
                         .boxed()
                         .flatMap(k -> IntStream.range(0, columnDataTypesB.size())
+                                .filter(l-> notInIgnoreColumnNames(tables.get(finalI).columns().get(k),tables.get(finalJ).columns().get(l)))
                                 .filter(l -> columnDataTypesA.get(k).equals(columnDataTypesB.get(l)) && !ignoreDatatypeList.contains(columnDataTypesA.get(k)))
                                 .mapToObj(l -> {
                                     boolean columnNameMatch = isColumnNameMatch(wordMatcher, tables.get(finalI).columns().get(k).toLowerCase(), tables.get(finalJ).columns().get(l).toLowerCase());
                                     double dataSimilarity = getJaccardIndex(tables.get(finalI).data(), tables.get(finalJ).data(), k, l);
-//                                    System.out.println("From Column: "+tables.get(finalI).columns().get(k)+" | To Column: "+tables.get(finalJ).columns().get(l)+ " | Jaccard Index: "+jaccardIndex);
-//                                    System.out.println("From Column: "+tables.get(finalI).columns().get(k)+" | To Column: "+tables.get(finalJ).columns().get(l)+ " | Match Status: "+columnNameMatch);
-//                                    System.out.println("From Column: "+tables.get(finalI).columns().get(k)+" | Uniqueness: "+tableAnalyzesResult.get(tableAName).getStreamResults()[k].getUniqueness());
-//                                    System.out.println("To Column: "+tables.get(finalJ).columns().get(l)+" | Uniqueness: "+tableAnalyzesResult.get(tableBName).getStreamResults()[l].getUniqueness());
                                     return new IntermediateRelationship(finalI, finalJ, k, l,columnNameMatch,dataSimilarity);
                                 } ))
                         .toList();
@@ -140,7 +175,12 @@ public class AutomaticRelationshipFinder<T> {
     private List<String> getColumnDataTypes(TextAnalysisResult[] analysisResult){
         List<String> columnDataTypes = new ArrayList<>();
         for (TextAnalysisResult result : analysisResult) {
-            columnDataTypes.add(result.getType().name());
+            if (isTypeBoolean(result)) {
+                columnDataTypes.add(DataTypes.BOOLEAN.name());
+            }
+            else {
+                columnDataTypes.add(result.getType().name());
+            }
         }
         return columnDataTypes;
     }
@@ -162,6 +202,28 @@ public class AutomaticRelationshipFinder<T> {
             columnData.add(row.get(index));
         }
         return columnData;
+    }
+    private boolean isTypeBoolean(TextAnalysisResult result){
+        if(result.getDistinctCount()==2){
+            if(result.getMinValue().equalsIgnoreCase("f") && result.getMaxValue().equalsIgnoreCase("t")){
+                return true;
+            }
+            else if(result.getMinValue().equalsIgnoreCase("n") && result.getMaxValue().equalsIgnoreCase("y")){
+                return true;
+            }
+            else return result.getMinValue().equalsIgnoreCase("no") && result.getMaxValue().equalsIgnoreCase("yes");
 
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean notInIgnoreColumnNames(String columnA, String columnB){
+        return ignoreColumnNamePatterns.stream()
+                .noneMatch(regex-> Pattern.compile(regex,Pattern.CASE_INSENSITIVE)
+                        .matcher(columnA).matches()) && ignoreColumnNamePatterns.stream()
+                .noneMatch(regex-> Pattern.compile(regex,Pattern.CASE_INSENSITIVE)
+                        .matcher(columnB).matches());
     }
 }
--- src/test/java/io/github/arf/AutomaticRelationshipFinderTest.java
+++ src/test/java/io/github/arf/AutomaticRelationshipFinderTest.java
@@ -10,52 +10,53 @@ public class AutomaticRelationshipFinderTest {
     @Test
     public void automaticRelationshipFinderTest(){
         String tableAName = "Customer";
-        List<String> columnNamesA = List.of("PK_Customer_ID","First", "Last", "Age");
+        List<String> columnNamesA = List.of("PK_Customer_ID","First", "Last", "Age","IsActive");
         List<List<String>> dataTableA = List.of(
-                List.of("1","Anaïs", "Nin","45"),
-                List.of("2","Gertrude", "Stein","52"),
-                List.of("3","Paul", "Campbell","20"),
-                List.of("4","Pablo", "Picasso","28"),
-                List.of("5","Theodore", "Camp","34"),
-                List.of("6","Henri", "Matisse","28"),
-                List.of("7","Georges", "Braque","54"),
-                List.of("8","Ernest", "Hemingway","55"),
-                List.of("9","Alice", "Toklas","45"),
-                List.of("10","Eleanor", "Roosevelt","28"),
-                List.of("11","Edgar", "Degas","47"),
-                List.of("12","Pierre-Auguste", "Wren","59"),
-                List.of("13","Claude", "Monet","28"),
-                List.of("14","Édouard", "Sorenson","25"),
-                List.of("15","Mary", "Dunning","88"),
-                List.of("16","Alfred", "Jones","77"),
-                List.of("17","Joseph", "Smith","41"),
-                List.of("18","Camille", "Pissarro","47"),
-                List.of("19","Franklin", "Roosevelt","57"),
-                List.of("20","Winston", "Churchill","78")
+                List.of("1","Anaïs", "Nin","45","n"),
+                List.of("2","Gertrude", "Stein","52","n"),
+                List.of("3","Paul", "Campbell","20","y"),
+                List.of("4","Pablo", "Picasso","28","n"),
+                List.of("5","Theodore", "Camp","34","y"),
+                List.of("6","Henri", "Matisse","28","n"),
+                List.of("7","Georges", "Braque","54","y"),
+                List.of("8","Ernest", "Hemingway","55","n"),
+                List.of("9","Alice", "Toklas","45","y"),
+                List.of("10","Eleanor", "Roosevelt","28","n"),
+                List.of("11","Edgar", "Degas","47","y"),
+                List.of("12","Pierre-Auguste", "Wren","59","n"),
+                List.of("13","Claude", "Monet","28","y"),
+                List.of("14","Édouard", "Sorenson","25","y"),
+                List.of("15","Mary", "Dunning","88","n"),
+                List.of("16","Alfred", "Jones","77","n"),
+                List.of("17","Joseph", "Smith","41","y"),
+                List.of("18","Camille", "Pissarro","47","n"),
+                List.of("19","Franklin", "Roosevelt","57","y"),
+                List.of("20","Winston", "Churchill","78","n")
         );
         Table<String> tableA = new Table<>(tableAName,columnNamesA,dataTableA);
         String tableBName = "Orders";
-        List<String> columnNamesB = List.of("ORDER_ID","Quantity", "Product_ID", "FK_Customer_ID");
+        List<String> columnNamesB = List.of("ORDER_ID","Quantity", "Product_ID", "FK_Customer_ID","Is_Active");
         List<List<String>> dataTableB = List.of(
-                List.of("OD001","2","P002","1"),
-                List.of("OD003","20","P012","2"),
-                List.of("OD003","28","P008","8"),
-                List.of("OD004","8","P009","3"),
-                List.of("OD005","14","P022","4"),
-                List.of("OD006","17","P109","5"),
-                List.of("OD007","1","P700","13"),
-                List.of("OD008","3","P802","7"),
-                List.of("OD009","7","P052","11"),
-                List.of("OD010","1","P092","12"),
-                List.of("OD011","1","P042","18"),
-                List.of("OD012","3","P072","19")
+                List.of("OD001","2","P002","1","n"),
+                List.of("OD003","20","P012","2","y"),
+                List.of("OD003","28","P008","8","n"),
+                List.of("OD004","8","P009","3","y"),
+                List.of("OD005","14","P022","4","n"),
+                List.of("OD006","17","P109","5","y"),
+                List.of("OD007","1","P700","13","y"),
+                List.of("OD008","3","P802","7","y"),
+                List.of("OD009","7","P052","11","y"),
+                List.of("OD010","1","P092","12","n"),
+                List.of("OD011","1","P042","18","n"),
+                List.of("OD012","3","P072","19","y")
         );
         Table<String> tableB = new Table<>(tableBName,columnNamesB,dataTableB);
         List<Table<String>> tables = List.of(tableA, tableB);
-
+        List<String> ignoreColumPatterns = List.of("is.*");
         AutomaticRelationshipFinder<String> relationshipFinder = new AutomaticRelationshipFinder.AutomaticRelationshipFinderBuilder<>(tables)
                 .setColumnNameConfidence(0.4)
-                .setColumnNameConfidence(0.5)
+                .setDataConfidence(0.5)
+                .setIgnoreColumnNamePatterns(ignoreColumPatterns)
                 .builder();
         List<Relationship> relationships = relationshipFinder.findRelationShip();
         relationships.forEach(relationship -> System.out.println("From Table: "+relationship.fromTable()+" | To Table: "+relationship.toTable()+" | From Column: "+relationship.fromColumnName()+" | To Column: "+relationship.toColumnName()));
